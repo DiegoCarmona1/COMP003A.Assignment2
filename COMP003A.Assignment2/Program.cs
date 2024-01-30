b6664fb6e101353e008b543c3759c191f7bf8ca4
Diego Carmona <diegocarmona@my.whccd.edu>
2024-01-30 02:43:17 -0800

    Added console queries for numerical and string inputs from the user.

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -16,16 +16,22 @@ namespace COMP003A.Assignment2
             Console.ForegroundColor = ConsoleColor.White;
 
             // String Section
+            Console.WriteLine("Input First Name");
             string firstName = Console.ReadLine();
+            Console.WriteLine("Input Middle Name");
             string middleName = Console.ReadLine();
+            Console.WriteLine("Input Last Name");
             string lastName = Console.ReadLine();
+            Console.WriteLine("Input Age in 2023");
             string inputAge = Console.ReadLine();
             int yearBorn = 2023 - Convert.ToInt32(inputAge);
             Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");
-            
+
             //Math Section
-            string num1 = "5";
-            string num2  = "6";
+            Console.WriteLine("Input Number");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Input Secondary Number");
+            string num2 = Console.ReadLine();
             int integer1 = Convert.ToInt32(num1);
             int integer2 = Convert.ToInt32(num2);
             Console.WriteLine($"{integer1} + {integer2} = {integer1 + integer2}");
@@ -35,11 +41,12 @@ namespace COMP003A.Assignment2
             Console.WriteLine($"{integer1} % {integer2} = {integer1 % integer2}");
 
             //Circle Area and Circumference Calculator Section
+            Console.WriteLine("Input radius of circle");
             double radius = Convert.ToDouble( Console.ReadLine() );
             double area = Math.PI * Math .Pow( radius, 2 );
             double circumference = 2 * Math.PI * radius;
-            Console.WriteLine(area);
-            Console.WriteLine(circumference);
+            Console.WriteLine($"Your area of the circle is {area}.");
+            Console.WriteLine($"Your circle's circumference is {circumference}.");
         }
     }
 }
